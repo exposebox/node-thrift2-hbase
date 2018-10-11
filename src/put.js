a92eb41de0120e1c143379acf81218aef5d8a027
Orel <orel@exposebox.com>
2018-10-11 16:56:00 +0300

    @put: converted to a proper Class added addObject method which adds all object properties as qualifiers in the Put

--- src/put.js
+++ src/put.js
@@ -3,27 +3,34 @@
 var Int64 = require('node-int64');
 const serde = require('./serde');
 
-function Put(row) {
-    if (!(this instanceof Put)) {
-        return new Put(row);
+class Put {
+    constructor(row) {
+        this.row = row;
+        this.columns = [];
     }
-    this.row = row;
-    this.columns = [];
+
+    add(family, qualifier, value, timestamp) {
+        var familyMap = {};
+        familyMap.family = family;
+        familyMap.qualifier = qualifier;
+        familyMap.value = serde.serialize(value);
+
+        if (timestamp) {
+            familyMap.timestamp = new Int64(timestamp);
+        }
+
+        this.columns.push(familyMap);
+        return this;
+    }
+
+    addObject(family, object, timestamp) {
+        for (const prop in object) {
+            const value = object[prop];
+            if (value !== undefined && value !== null)
+                put.add(cf, prop, value, timestamp);
+        }
+    }
 }
-
-Put.prototype.add = function (family, qualifier, value, timestamp) {
-    var familyMap = {};
-    familyMap.family = family;
-    familyMap.qualifier = qualifier;
-    familyMap.value = serde.serialize(value);
-
-    if (timestamp) {
-        familyMap.timestamp = new Int64(timestamp);
-    }
-
-    this.columns.push(familyMap);
-    return this;
-};
 
 
 module.exports = Put;
