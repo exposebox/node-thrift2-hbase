d53e4a512235c6fa33333c5fb8530805b2167ed0
Orel <orel@exposebox.com>
2018-10-14 17:46:40 +0300

    @put: fixes to addObject

--- src/put.js
+++ src/put.js
@@ -1,6 +1,7 @@
 "use strict";
 
-var Int64 = require('node-int64');
+const Int64 = require('node-int64');
+
 const serde = require('./serde');
 
 class Put {
@@ -26,8 +27,17 @@ class Put {
     addObject(family, object, timestamp) {
         for (const prop in object) {
             const value = object[prop];
-            if (value !== undefined && value !== null)
-                put.add(cf, prop, value, timestamp);
+            if (value !== undefined && value !== null) {
+                if (Array.isArray(value)) {
+                    this.add(family, prop, {type: 'json', value}, timestamp);
+                }
+                else if (typeof value === 'object') {
+                    this.addObject(family, value, timestamp);
+                }
+                else {
+                    this.add(family, prop, value, timestamp);
+                }
+            }
         }
     }
 }
